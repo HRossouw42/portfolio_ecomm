7d0ac9584c511ea3cea7f01067adf113ded1e42d
Harmun <harmun.rossouw@coil.com>
2022-03-30 14:05:09 +0200

    login flow

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
 
 const app = express(); // instance of express
 app.use(bodyParser.urlencoded({ extended: true })); // employs body parcer globally as middleware
+app.use(
+  cookieSession({
+    keys: ['devdogdevdogdevdogdev'],
+  })
+);
 
 const log = console.log.bind(console);
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
   <div>
+    Your id is: ${req.session.userId}
     <form method="POST" >
       <input name="email" placeholder="email" />
       <input name="password" placeholder="password" />
@@ -19,9 +27,63 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  log(req.body);
+// Create an account
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email already in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
+  // Create user in user repo
+  const user = await usersRepo.create({ email, password });
+
+  // Store id of user inside user cookie
+  req.session.userId = user.id; // added by cookie-session library
+
   res.send('Account created!');
+});
+
+app.get('/signout', (req, res) => {
+  // clear cookie-session
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST" >
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <button>Sign In</button>
+    </form>
+  </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('Invalid password');
+  }
+
+  // assign cookie
+  req.session.userId = user.id;
+
+  res.send('You are now signed in!');
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   // check and see if filename given
@@ -18,22 +19,88 @@ class UsersRepository {
   }
 
   // -- METHODS --
+  // -------------
+
   async getAll() {
-    // open file called x.filename
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf8',
-    });
-    // read contents
-    console.log(contents);
-    // parse contents
-    // return parsed data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async create(attrs) {
+    // random id
+    attrs.id = this.randomId();
+
+    const records = await this.getAll();
+    records.push(attrs);
+
+    // write record array back to this.filename
+    await this.writeAll(records);
+
+    // return attrs object for login purposes
+    return attrs;
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((records) => records.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((records) => records.id === id);
+
+    if (!record) {
+      throw new Error(`>> Record with id of ${id} not found`);
+    }
+
+    // Object.assign method copies all enumerable own properties from source objects to a target object
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  // filters is an object containing what to look for
+  // get all records
+  // go through records and
+  // see if they match any key in filters
+  // if found return record
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
   }
 }
 
-// TESTS
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-  await repo.getAll();
-};
-
-test();
+module.exports = new UsersRepository('users.json');
